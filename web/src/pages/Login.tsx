0b5745c0973cf22bc49392e2f4b3054cf68cf210
Arthur Conti <arthurgallotticonti@gmail.com>
2020-12-20 01:40:58 -0300

    inicio do happy 2.0

---
+++ web/src/pages/Login.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import '../styles/pages/login.css'
+
+
+
+function Login() {
+
+    return (
+        <div id="login">
+            <div className="content-wrapper">
+                <div className="local-form">
+
+                </div>
+
+            </div>
+        </div>
+    );
+
+
+}
+
+
+export default Login
--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -5,6 +5,7 @@ import Landing from './pages/Landing'
 import Orphanage from './pages/Orphanage';
 import OrphanageCreate from './pages/CreateOrphanage';
 import OrphanagesMap from './pages/OrphanagesMap'
+import Login from './pages/Login';
 
 function Routes() {
     return (
@@ -12,9 +13,11 @@ function Routes() {
             <Switch>
                 <Route path="/" exact component={Landing} />
                 <Route path="/app" component={OrphanagesMap} />
-                
+
                 <Route path="/orphanage/create" component={OrphanageCreate} />
                 <Route path="/orphanage/:id" component={Orphanage} />
+                
+                <Route path="/login" component={Login} />
             </Switch>
         </BrowserRouter>
     );
