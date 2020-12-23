4cfebe1802628e3a4504dc409e8b065c18752d7c
Arthur Conti <arthurgallotticonti@gmail.com>
2020-12-23 14:06:16 -0300

    alterações tela login

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import '../styles/pages/login.css'
 
+import LogoHappy from '../images/Logotipo.svg'
+
 
 
 function Login() {
@@ -8,6 +10,17 @@ function Login() {
     return (
         <div id="login">
             <div className="content-wrapper">
+
+                <div className="presentation-container">
+                    <img src={LogoHappy} alt="logo happy" />
+
+                    <div className="location">
+                        <strong>Mirassol</strong>
+                        <span>São Paulo</span>
+                    </div>
+                    
+                </div>
+
                 <div className="local-form">
 
                 </div>
