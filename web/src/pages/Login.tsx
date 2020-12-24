2f4ca46c71d001c8911ddcb6bcbd7c9c18e29091
Arthur Conti <arthurgallotticonti@gmail.com>
2020-12-23 21:44:54 -0300

    Alteracoes tela Login formulario inicio

--- web/src/pages/Login.tsx
+++ web/src/pages/Login.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { FormEvent } from 'react'
+import { Link } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi'
 import '../styles/pages/login.css'
 
 import LogoHappy from '../images/Logotipo.svg'
@@ -7,6 +9,13 @@ import LogoHappy from '../images/Logotipo.svg'
 
 function Login() {
 
+    function handleOnSubmit(event: FormEvent) {
+        event.preventDefault()
+
+
+    }
+
+
     return (
         <div id="login">
             <div className="content-wrapper">
@@ -14,15 +23,39 @@ function Login() {
                 <div className="presentation-container">
                     <img src={LogoHappy} alt="logo happy" />
 
-                    <div className="location">
+                    <div className="text-location">
                         <strong>Mirassol</strong>
                         <span>São Paulo</span>
                     </div>
-                    
+
                 </div>
 
                 <div className="local-form">
 
+                    <Link to="/" className="back-root">
+                        <FiArrowLeft size={24} color="#15C3D6" />
+                    </Link>
+                    <form onSubmit={handleOnSubmit}>
+                        <h2>Fazer Login</h2>
+                        <br />
+                        <label htmlFor="email">E-mail</label>
+                        <input id="email" type="email" placeholder="exemplo@exemplo.com" />
+                        <br />
+
+                        <label htmlFor="senha">Senha</label>
+                        <input id="senha" type="password" placeholder="Digite sua senha" />
+                        <br />
+                        <div className="checkbox-content">
+                            <div className="checkbox-label">
+                                <input type="checkbox" id="remember" name="remember" value="remember" className="check-style"/>
+                                <label htmlFor="remember"> Lembrar-me</label>
+                            </div>
+
+                            <Link to="/">Esqueci minha senha</Link>
+                        </div>
+                        <br />
+                        <button type="submit"> Entrar</button>
+                    </form>
                 </div>
 
             </div>
